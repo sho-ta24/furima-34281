2ee086ae4560d2a73fbc4579e57eb475c52aebc6
sho-ta24 <ushota201122@gmail.com>
2021-05-26 12:10:46 +0900

    新規登録、ログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]+\z/}
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format:{ with:/\A[あ-んア-ン一-龥]+\z/}
+  validates :first_name, presence: true, format:{ with:/\A[あ-んア-ン一-龥]+\z/}
+  validates :last_name_kana, presence: true, format:{ with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format:{ with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
 end
