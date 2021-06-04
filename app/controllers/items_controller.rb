9eff8739fea0b9bc1e1184cdee3109e8b0268426
sho-ta24 <ushota201122@gmail.com>
2021-06-05 06:10:08 +0900

    出品ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
+
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
   def index
+  end
+
+  def new 
+  end
+
+
+
+  private
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to  "/users/sign_in"
+    end
   end
 end
