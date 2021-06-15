1cb02b31503e85c5f3ea7f21d0681ee8b2aaaff0
sho-ta24 <ushota201122@gmail.com>
2021-06-15 20:41:48 +0900

    バリデーション追加

---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :sales_status_id, numericality: { other_than: 1 } 
+    validates :shipping_free_status_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :scheduled_delivery_id, numericality: { other_than: 1 } 
+    validates :price format:{ with:/\A[0-9]+\z/}, numericality: { in: 300..9_999_999 }
+  end
+  has_one_attached :image
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :password, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]+\z/}
-    with_options presence: true do
+  with_options presence: true do
     validates :nickname
     validates :last_name, format:{ with:/\A[あ-んア-ン一-龥]+\z/}
     validates :first_name, format:{ with:/\A[あ-んア-ン一-龥]+\z/}
