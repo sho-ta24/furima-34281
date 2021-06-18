eca68648c4024bc78692ae18df0505251d513204
sho-ta24 <ushota201122@gmail.com>
2021-06-18 20:03:30 +0900

    出品機能テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,11 @@
-
 class ItemsController < ApplicationController
   before_action :move_to_signed_in, except: [:index]
   def index
   end
 
-  def new 
+  def new
     @item = Item.new
   end
-
 
   def create
     @item = Item.new(item_params)
@@ -18,17 +16,14 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   private
+
   def move_to_signed_in
-    unless user_signed_in?
-      redirect_to  "/users/sign_in"
-    end
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_free_status_id, :prefecture_id, :scheduled_delivery_id , :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_free_status_id, :prefecture_id,
+                                 :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
-
