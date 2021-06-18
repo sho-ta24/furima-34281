1e8a3da7d4464f1565f15539c8866510fba3c354
sho-ta24 <ushota201122@gmail.com>
2021-06-18 21:05:49 +0900

    バリデーション共通化

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :category_id, numericality: { other_than: 1 }
-    validates :sales_status_id, numericality: { other_than: 1 }
-    validates :shipping_free_status_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :scheduled_delivery_id, numericality: { other_than: 1 }
+    ith_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_free_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
     validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9_999_999 }
   end
   has_one_attached :image
