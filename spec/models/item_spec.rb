88400321a998fd90f62387f233d44861295ed3f4
sho-ta24 <ushota201122@gmail.com>
2021-06-19 15:32:44 +0900

    クオーテーションを削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Item, type: :model do
       end
 
       it 'priceが¥9,999,999より高いと登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
