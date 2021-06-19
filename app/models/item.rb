b4eedd5b4c359922e92d0eacf752a10361429c13
sho-ta24 <ushota201122@gmail.com>
2021-06-19 15:37:45 +0900

    active_hashのテーブルとのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,11 @@ class Item < ApplicationRecord
   end
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_free_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
