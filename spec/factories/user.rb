3a9e207021d7e978560a12feefdfb50e22ff62ed
sho-ta24 <ushota201122@gmail.com>
2021-06-19 20:57:55 +0900

    password英数字混合

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     encrypted_password   { password }
     first_name           { '山田' }
     first_name_kana      { 'ヤマダ' }
