6577f9041fd7f3b73a94bcd7f54c3079dbb0ea6d
sho-ta24 <ushota201122@gmail.com>
2021-07-25 13:06:32 +0900

    バリデーション修正

--- app/models/management_street_address.rb
+++ app/models/management_street_address.rb
@@ -1,22 +1,24 @@
-class ManagementStreetAddress < ApplicationRecord
+class ManagementStreetAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :addresses, :building, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
 
   with_options presence: true do
-    validates :post_num , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :banch 
+    validates :postal_code , format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
-    validates :phone_num , format: {with: /\A[0-9]{11}\z/}
+    validates :phone_number , format: {with: /\A[0-9]{11}\z/}
+    validates :address
     validates :item_id
     validates :user_id
   end
-  validates :prefecture, numericality: {other_than: 1}
+  validates :prefecture_id, numericality: {other_than: 1}
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   def save
-
     # 購入者情報を保存
     management = Management.create(user_id: user_id, item_id: item_id)
     # 配送先住所の情報を保存
-    StreetAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, phone_num: phone_num, building: building, order_id: order.id)
+    StreetAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, building: building, management_id: management.id)
   end
 end
+
+
