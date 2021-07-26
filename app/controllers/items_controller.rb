7a7a51e3d59eebf62d2ad55137e763d76a93e116
sho-ta24 <ushota201122@gmail.com>
2021-07-26 21:39:20 +0900

    before_action追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :item_user, only: [:edit, :update, :destroy]
-  before_action :order_id, only: [:edit]
+  before_action :order_id, only: [:edit, :update, :destroy]
 
 
   def index
